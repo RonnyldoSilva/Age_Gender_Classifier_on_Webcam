a045f48efb803ff4ddd353f4ff42a9d1e7e421c6
Ronnyldo Silva <37785171+RonnyldoSilva@users.noreply.github.com>
2019-04-11 23:34:55 -0300

    Add files via upload

---
+++ CMakeFiles/AgeGender.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AgeGender.dir/AgeGender.cpp.o"
+  "AgeGender.pdb"
+  "AgeGender"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AgeGender.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
